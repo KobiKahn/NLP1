907cac56c70d40673cc814ec52f07f209b92cdf6
kobi <kobikahn@gmail.com>
2021-09-27 09:40:28 -0400

    1

--- main.py
+++ main.py
@@ -15,5 +15,59 @@ def open_file(filename, mode = 'read'):
         else:
             text = file.readline()
     return text
-open = open_file('file1.txt')
-print(open)
+# open = open_file('file1.txt')
+# print(open)
+
+def print_file(filename):
+    file_ = open_file(filename)
+    print(file_)
+
+# print_file('file1.txt')
+
+def sumColumn(filename):
+    total = 0
+    read_int = open_file(filename, 'readlines')
+    for int_ in read_int:
+        total += int(int_)
+    print(total)
+# sumColumn('int.txt')
+
+def sum_all(filename):
+    total = 0
+    int_list = []
+    read_int = open_file(filename)
+    for num in read_int:
+        if num.isdigit():
+            total += int(num)
+    print(total)
+
+# sum_all('int2.txt')
+
+def read_column(filename, columnNo = 0):
+    list_c = []
+    columns = open_file(filename, 'readlines')
+    for row in columns:
+        row = row.split()
+        list_c.append(row[columnNo])
+    print(list_c)
+
+# read_column('readC.txt')
+word_list = ['better', 'honking']
+def countword(filename, words):
+    count = 0
+    word_list = open_file(filename)
+    word_list = word_list.lower().split()
+
+    for word in word_list:
+        for letter in word:
+            if not letter.isalpha():
+                word_list.remove(letter)
+    print(word_list)
+        # if word == words[0]:
+        #     count += 1
+
+    print(count)
+countword('bigwords.txt', word_list)
+
+
+
