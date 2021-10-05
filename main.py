8ea1be12726a00dc2d328a750014fc37b217fbcd
kobi <kobikahn@gmail.com>
2021-10-05 09:38:48 -0400

    1

--- main.py
+++ main.py
@@ -72,9 +72,85 @@ def countword(filename, words):
 # countword('bigwords.txt', word_list)
 
 
+
+
+
+
+
+
+
+
+
+
+##### PROJECT DATABASE!!!
+
+
+
+
+
+dict_g = {}
+dict_a = {}
+dict_start = {}
+dict_s = {}
+dict_d = {}
+
+gender_l = []
+age_l = []
+start_l = []
+salary_l = []
+dept_l = []
+
+x = 0
 with open('Company_DB - Sheet1.csv') as db:
     company_db = csv.reader(db)
 
     for row in company_db:
-        print(row)
+        x += 1
+        if x != 1:
+            dict_g[f'{row[0] , row[1]}'] = row[2]
+            dict_a[f'{row[0] , row[1]}'] = row[3]
+            dict_start[f'{row[0] , row[1]}'] = row[4]
+            dict_s[f'{row[0] , row[1]}'] = row[5]
+            dict_d[f'{row[0] ,row[1]}'] = row[6]
 
+
+
+# print(dict_g)
+# print(dict_a)
+# print(dict_start)
+# print(dict_s)
+# print(dict_d)
+question = int(input('		To get information about employee age type:  1\n		To get information about employee gender type:  2\n		To get information about employee salary type: 3\n		To get information about employee start date type: 4\n		To get information about employee department type: 5\n		To exit the database query type: 6\nWhat kind of information do you wish to extract : '))
+print()
+print()
+print()
+print()
+print()
+if question == 6:
+    print('Adios!')
+
+elif question == 1:
+
+    print('Please have end age greater than start age')
+    age_start = int(input('Input the start age: '))
+    age_end = int(input('Input the end age: '))
+    print()
+    print()
+
+
+
+    for key, value in dict_a.items():
+        if int(value) >= age_start and int(value) <= age_end:
+            print(key, value)
+
+    # print(age_vals)
+
+
+
+
+
+# print(dict_g)
+# print(dict_a)
+# print(dict_start)
+# print(dict_s)
+# print(dict_d)
