333c16bb196f214ebfd95caf602d6a371a4b34e9
kobi <kobikahn@gmail.com>
2021-11-08 09:40:07 -0500

    1

--- main.py
+++ main.py
@@ -74,33 +74,109 @@ def calculate(month, day, year):
 
 
 def evaluation(main_query, final_dict):
-    main_query = main_query.split()
+    global and_
 
-    token1 = main_query[0]
-    token2 = main_query[1]
-    token3 = main_query[2]
 
-    if token2 == '=':
-        token2 = '=='
+    x = 0
 
-    if token1.lower() == 'startdate':
-        token3 = token3.split('/')
-        user_total = calculate(token3[0], token3[1], token3[2])
+    for item in main_query:
+        x += 1
 
-        for item in final_dict:
-            data_date = item[token1]
+    if x == 1:
+        main_query = main_query.split()
+        token1 = main_query[0]
+        token2 = main_query[1]
+        token3 = main_query[2]
 
-            # print(data_date)
-            data_total = calculate(data_date[0], data_date[1], data_date[2])
-            # print(f'user: {user_total}')
-            # print(f'comp: {data_total}')
-            if eval(f'"{data_total}" {token2} "{user_total}"'):
-                print(item)
+        if token2 == '=':
+            token2 = '=='
+
+        if token1.lower() == 'startdate':
+            token3 = token3.split('/')
+            user_total = calculate(token3[0], token3[1], token3[2])
+
+            for item in final_dict:
+                data_date = item[token1]
+
+                # print(data_date)
+                data_total = calculate(data_date[0], data_date[1], data_date[2])
+                # print(f'user: {user_total}')
+                # print(f'comp: {data_total}')
+                if eval(f'"{data_total}" {token2} "{user_total}"'):
+                    print(item)
+
+        else:
+            for item in final_dict:
+                if eval(f'"{item[token1].lower()}" {token2} "{token3}"'):
+                    print(item)
+
 
     else:
-        for item in final_dict:
-            if eval(f'"{item[token1].lower()}" {token2} "{token3}"'):
-                print(item)
+        if and_:
+            # print(main_query)
+            list1 = main_query[0].split()
+            list2 = main_query[1].split()
+
+            token1 = list1[0]
+            token2 = list1[1]
+            token3 = list1[2]
+
+            token4 = list2[0]
+            token5 = list2[1]
+            token6 = list2[2]
+
+            if token2 == '=':
+                token2 = '=='
+
+            if token5 == '=':
+                token5 = '=='
+
+            if token1.lower() == 'startdate' and token4.lower() == 'startdate':
+                print('HIIII')
+                token3 = token3.split('/')
+                query1_total = calculate(token3[0], token3[1], token3[2])
+
+                token6 = token6.split('/')
+                query2_total = calculate(token6[0], token6[1], token6[2])
+
+
+                for item in final_dict:
+                    data_date = item[token1]
+
+                    # print(data_date)
+                    data_total = calculate(data_date[0], data_date[1], data_date[2])
+                    # print(f'user: {user_total}')
+                    # print(f'comp: {data_total}')
+
+                    # print(data_date)
+
+
+                    if eval(f'"{data_total}" {token2} "{query1_total}"') and eval(f'"{data_total}" {token5} "{query2_total}"'):
+                        print(item)
+
+            # elif token1.lower() == 'startdate':
+
+
+
+
+            if token4.lower() == 'startdate':
+                token6 = token6.split('/')
+                query2_total = calculate(token6[0], token6[1], token6[2])
+
+                for item in final_dict:
+                    data_date2 = item[token4]
+
+                    # print(data_date)
+                    data_total2 = calculate(data_date2[0], data_date2[1], data_date2[2])
+                    # print(f'user: {user_total}')
+                    # print(f'comp: {data_total}')
+                    if eval(f'"{data_total2}" {token2} "{query2_total}"'):
+                        print(item)
+
+
+
+
+
 
     main_query = main(valid_queries, valid_dict, dept_vals)
     if main_query:
@@ -141,6 +217,7 @@ def test1(token1,valid_queries):
 def test2(token1, token2, valid_dict):
     global valid
     value_c = 0
+
     while value_c == 0:
         for key, value in valid_dict.items():
             if key == token1.lower():
@@ -179,7 +256,7 @@ def test3(token1, token3, department_vals, gender_vals):
 
     elif token1.lower() == 'department':
         for value in department_vals:
-
+            print(value)
             if value.lower() == token3.lower():
                 valid = True
                 break
@@ -217,6 +294,13 @@ def transition(token4):
 def main(valid_queries, valid_dict, department_vals):
     global valid
     global user_string_query
+    global and_
+
+    and_ = False
+
+
+    user_string_query = []
+
     valid = True
     token1 = 0
     counter_or = 0
@@ -244,32 +328,43 @@ def main(valid_queries, valid_dict, department_vals):
         token6 = user_query[5]
         token7 = user_query[6]
 
-        if valid and transition(token4) == 'or':
+        query1 = [token1, token2, token3]
+        query2 = [token5, token6, token7]
+
+        parent = [query1, query2]
+
+        if transition(token4) == 'or':
+
             if test1(token1, valid_queries) or test1(token5, valid_queries):
                 counter_or = 1
                 if test2(token1, token2, valid_dict) or test2(token5, token6, valid_dict):
                     counter_or = 2
                     if test3(token1, token3, department_vals, gender_vals) or test3(token5, token7, department_vals,gender_vals):
                         counter_or = 3
-                        user_string_query = finish(valid, user_query)
+                        user_string_query = []
+                        user_string_query.append(finish(valid, user_query))
+                        print(f'USER_STRING {user_string_query}')
 
             if counter_or != 3:
                 finish(valid)
 
 
 
-        elif valid and transition(token4) == 'and':
+        elif transition(token4) == 'and':
+
+            and_ = True
 
             if test1(token1, valid_queries) and test1(token5, valid_queries):
                 counter_and = 1
                 if test2(token1, token2, valid_dict) and test2(token5, token6, valid_dict):
                     counter_and = 2
-                    if test3(token1, token3, department_vals, gender_vals) and test3(token5, token7,department_vals,gender_vals):
+                    if test3(token1, token3, department_vals, gender_vals) and test3(token5, token7, department_vals, gender_vals):
                         counter_and = 3
-                        user_string_query = finish(valid, user_query)
-                        print(user_string_query)
 
+                        for item in parent:
+                            user_string_query.append(finish(valid, item))
 
+                        return(user_string_query)
 
             if counter_and != 3:
                 finish(valid)
@@ -289,13 +384,17 @@ def main(valid_queries, valid_dict, department_vals):
             pass
         if valid and test3(token1, token3, department_vals, gender_vals):
             size3_counter = 3
+
+
+
+
             user_string_query = finish(valid, user_query)
+            return user_string_query
 
         if size3_counter != 3:
             finish(valid)
 
-        if valid:
-            return user_string_query
+
 
     elif valid:
         print('ERROR WITH THE LENGTH OF QUERY')
@@ -309,7 +408,7 @@ def main(valid_queries, valid_dict, department_vals):
 
 main_query = main(valid_queries, valid_dict, dept_vals)
 # main_query = main_query.split()
-print(main_query)
+print(f'main_query {main_query}')
 
 if main_query:
     evaluation(main_query, final_dict)
